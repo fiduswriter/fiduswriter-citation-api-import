7234f8484b2e213ae0bad7781152f1ade9f3d714
Johannes Wilm <mail@johanneswilm.org>
2020-09-21 05:16:55 +0200

    3.8.2

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='fiduswriter-citation-api-import',
-    version='3.8.1',
+    version='3.8.2',
     packages=find_namespace_packages(),
     include_package_data=True,
     exclude_package_data={
