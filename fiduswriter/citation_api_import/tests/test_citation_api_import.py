f0527f47b18571e9f9cd671458541893ef75240b
Johannes Wilm <mail@johanneswilm.org>
2026-05-30 17:06:45 +0200

    Update test server location

--- fiduswriter/citation_api_import/tests/test_citation_api_import.py
+++ fiduswriter/citation_api_import/tests/test_citation_api_import.py
@@ -3,7 +3,7 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from channels.testing import ChannelsLiveServerTestCase
+from testing.live_server import ChannelsLiveServerTestCase
 from testing.selenium_helper import SeleniumHelper
 
 
